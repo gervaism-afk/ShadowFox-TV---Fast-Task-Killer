89a81c59b8ac9763b598e961500f15e7f688eb81
gervaism-afk <gervaism@gmail.com>
2026-09-21 16:24:15 -0400

    Fix Compose border import for v6.8.13 result slot

--- overrides/app/src/main/java/ca/shadowfoxtv/taskkiller/MainActivity.kt
+++ overrides/app/src/main/java/ca/shadowfoxtv/taskkiller/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
